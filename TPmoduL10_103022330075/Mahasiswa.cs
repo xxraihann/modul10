1638aa7abf1611d2b2d65384b58aeb801df439f4
xxraihann <raihanananda1980@gmail.com>
2025-04-26 15:27:04 +0700

    menambahkan class mahasiswa dan controller

---
+++ TPmoduL10_103022330075/Mahasiswa.cs
@@ -0,0 +1,8 @@
+﻿namespace TPmoduL10_103022330075
+{
+    public class Mahasiswa
+    {
+        public string Nama { get; set; }
+        public string Nim { get; set; }
+    }
+}
---
+++ TPmoduL10_103022330075/MahasiswaController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TPmoduL10_103022330075; 
+using System.Collections.Generic;
+using TPmoduL10_103022330075;
+
+namespace WebApiMahasiswa.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MahasiswaController : ControllerBase
+    {
+        private static readonly List<Mahasiswa> daftarMahasiswa = new List<Mahasiswa>
+        {
+            new Mahasiswa { Nama = "Raihan Ananda Putra", Nim = "133022330075" },
+            new Mahasiswa { Nama = "Intan Nur Aini", Nim = "13302233" },
+            new Mahasiswa { Nama = "Gumi", Nim = "13302233" }
+        };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Mahasiswa>> Get()
+        {
+            return daftarMahasiswa;
+        }
+    }
+}
